6e1160b263510540305818cfa78c4fdc1f5f73b5
being2 <129958950+beingtwo@users.noreply.github.com>
2024-02-08 15:43:49 +0900

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Watching bypassed ez","Listening to bypassed ezy","Playing bypassed ez"];
 
 
 let currentIndex = 0;
